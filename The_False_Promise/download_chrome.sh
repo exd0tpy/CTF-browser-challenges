e9a2540ed7b8d993ed5d90fcc7e26706a4fbcd8d
exd0tpy <jsw5258@ajou.ac.kr>
2021-04-19 07:55:54 +0000

    add challenge

---
+++ The_False_Promise/download_chrome.sh
@@ -0,0 +1,11 @@
+export fileid=1HXl97SoM1KrIWnqSM2R87kGI0sVIUt3r 
+export filename=chrome.zip
+
+## WGET ##
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
+	     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+
+wget --load-cookies cookies.txt -O $filename \
+	     'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
+
+rm -f confirm.txt cookies.txt
